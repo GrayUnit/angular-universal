93ef4f61e24d012fa949b675a4c14154e842ecf5
GrayUnit <grayunit@outlook.fr>
2020-10-16 15:41:59 +0200

    angular coté serveur avec webpack

---
+++ src/server.ts
@@ -0,0 +1,39 @@
+
+import { renderModuleFactory } from '@angular/platform-server';
+import * as express from 'express';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const AppServerModuleNg = require("../dist/server/main");
+
+const app = express();
+const template = readFileSync(join(
+  __dirname, "..", "dist", "browser", "index.html"
+)).toString();
+
+app.engine(
+  'html', (_, options, callback) => {
+    const opts = {
+      document: template,
+      url: options.req.url,
+      extraProviders: []
+    };
+    renderModuleFactory(AppServerModuleNg, opts).then(
+      (html) => {
+        return callback(null, html);
+      }
+    )
+  }
+);
+
+app.set('view engine', 'html');
+app.set('views', 'src');
+
+app.get('*', (req, res) => {
+  res.render('index', {req});
+});
+
+app.listen(4000, () => {
+  console.log("Server listening .....")
+});
+
